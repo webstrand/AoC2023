ea0191a5780a8d7b1d763046ecb6458e68049270
webstrand <373938+webstrand@users.noreply.github.com>
2023-12-03 19:43:54 -0500

    improve day 3 code structure

--- 3.scala
+++ 3.scala
@@ -6,17 +6,6 @@ implicit class Dumpable[A](x: A) {
 	def DUMP[B] = runtime.ScalaRunTime.replStringOf(x, 1000)
 }
 
-implicit class Product2Ops[A,B](x: Product2[A, B]) {
-	def iter = x.productIterator.map(_.asInstanceOf[A | B])
-}
-
-implicit class Product3Ops[A,B,C](x: Product3[A, B, C]) {
-	def iter = x.productIterator.map(_.asInstanceOf[A | B | C])
-}
-
-
-
-
 val data1 = Source.fromString(
 	"""467..114..
 	  |...*......
@@ -36,17 +25,16 @@ val data2 = Source.fromFile("3.input").getLines().toList
 case class PartNumber(val id: Int, val row: Int, val col: Int) {
 	override
 	def toString = s"PartNumber($id)"
-
 }
-case class Cell(val row: Int, val col: Int, val part: String) {}
+case class Part(val name: String, val row: Int, val col: Int) {}
 
 class Row(val columns: Vector[PartNumber | String]) extends Iterable[PartNumber | String] {
 	def iterator: Iterator[PartNumber | String] = columns.iterator
 	override
 	def stringPrefix = "Row"
 
-	def opposite(col: Int) = List(columns.lift(col - 1), columns.lift(col), columns.lift(col + 1))
-	def adjacent(col: Int) = List(columns.lift(col - 1), columns.lift(col + 1))
+	def xxx(col: Int) = List(columns.lift(col - 1), columns.lift(col), columns.lift(col + 1))
+	def x_x(col: Int) = List(columns.lift(col - 1), columns.lift(col + 1))
 }
 
 class Schema(val rows: Vector[Row]) extends Iterable[Row] {
@@ -54,39 +42,60 @@ class Schema(val rows: Vector[Row]) extends Iterable[Row] {
 	override
 	def stringPrefix = "Schema"
 
-	def findPartNumber(row: Int, col: Int) = 
+	def findAdjacentPartNumbers(row: Int, col: Int) = 
 		List(
-			rows.lift(row - 1).map(_.opposite(col)), 
-			Some(rows.apply(row).adjacent(col)), 
-			rows.lift(row + 1).map(_.opposite(col))
+			rows.lift(row - 1).map(_.xxx(col)), 
+			rows.lift(row    ).map(_.x_x(col)), 
+			rows.lift(row + 1).map(_.xxx(col)),
 		).flatten.flatten.flatten.collect { case a: PartNumber => a }
 
-	def parts = rows.map(_.columns.zipWithIndex.collect {
-		case (s: String, i) if s != "." => (s, i)
-	}).zipWithIndex.collect {
-		case (v, row) if v.length != 0 => v.map { case (part, col) => Cell(row, col, part) }
-	}.toList.flatten
+	def parts =
+		// Visit every row
+		rows.map(
+			// Visit every column and pick out only the part symbols
+			_.columns.zipWithIndex
+				.collect { case (s: String, i) if s != "." => (s, i) }
+		).zipWithIndex
+		.collect {
+			case (v, row) if v.length != 0 => 
+				v.map { case (part, col) => Part(part, row, col) }
+		}.toList.flatten
 }
 
-val tokenizer = raw"\d+|\D".r
-def parse(data: Iterable[String]) = data.zipWithIndex
-	.map { case (l, i) =>
-		tokenizer.findAllIn(l).zipWithIndex.flatMap {
-			case (x, j) if x.head.isDigit => List.fill(x.length)(PartNumber(x.toInt, i, j))
-			case (x, j) => List(x)
-		}
-		.pipe(x => new Row(x.toVector))
-	}.pipe(x => new Schema(x.toVector))
+object Schema {
+	private val tokenizer = raw"\d+|\D".r
 
-def part(schema: Schema) = schema
+	def fromLines(data: Iterable[String]) = 
+		data.zipWithIndex
+			.map { case (l, i) =>
+				tokenizer.findAllIn(l).zipWithIndex.flatMap {
+					case (x, j) if x.head.isDigit => List.fill(x.length)(PartNumber(x.toInt, i, j))
+					case (x, j) => List(x)
+				}
+				.pipe(x => new Row(x.toVector))
+			}.pipe(x => new Schema(x.toVector))
+}
+
+def solution1(data: Iterable[String]) =
+	val schema = Schema.fromLines(data)
+	// for each part, find all of the adjacent PartNumbers
+	schema.parts.map(c => schema.findAdjacentPartNumbers(c.row, c.col))
+		// merge the lists and avoid counting the same PartNumber twice
+		.flatten.distinct
+		.map(x => x.id)
+		.sum
 
 
+def solution2(data: Iterable[String]) = 
+	val schema = Schema.fromLines(data)
+	// for each part, find all of the _distinct_ adjacent PartNumbers. 
+	schema.parts.map(c => schema.findAdjacentPartNumbers(c.row, c.col).distinct)
+		// find only the parts with exactly two adjacent PartNumbers
+		.collect { case l if l.length == 2 => l }
+		// get the product of those two adjacent numbers
+		.map(l => l.foldLeft(1)(_ * _.id))
+		.sum
 
-def solution1(data: Iterable[String]) = parse(data).pipe(schema => schema.parts.map(c => schema.findPartNumber(c.row, c.col))).flatten.distinct.map(x => x.id).sum
-
-def solution2(data: Iterable[String]) = parse(data).pipe(schema => 
-	schema.parts.map(c => schema.findPartNumber(c.row, c.col).distinct).collect { case l if l.length == 2 => l }.map(l => l.foldLeft(1)(_ * _.id)).sum
-)
 
 def main(args: Array[String]): Unit = {
 	println("part1-sample: " ++ solution1(data1).DUMP)
